50a8420e1a20991cc1a44e5879b7756ed723ef15
Shanmathi <k-shanmathi@inahnosolutions.com>
2023-04-25 15:57:49 +0530

    New code

---
+++ src/com/sample/Sample.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class Sample {
+	public void m1() {
+		System.out.println("Today is Tuesday");
+	}
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.m1();	
+	}
+}
